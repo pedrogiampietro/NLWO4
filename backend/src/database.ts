e441af9c84cfe4ed477d83721e51abef133bc9f6
Pedro Giampietro <pedrogiampietro@hotmail.com>
2021-02-23 17:39:29 -0300

    Added User Controller, Models, and use uuid to gerate Id;

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,18 +1,5 @@
-import 'reflect-metadata';
 import { createConnection } from 'typeorm';
-import config from './config';
-import path from 'path';
 
-export default async (): Promise<any> => {
-  const connection = await createConnection({
-    type: 'mysql',
-    host: config.typeorm.host,
-    port: config.typeorm.port,
-    username: config.typeorm.username,
-    password: config.typeorm.password,
-    database: config.typeorm.database,
-    migrations: ['src/migrations/**/*.ts'],
-  });
-
-  return connection.manager;
-};
+createConnection()
+  .then(async (connection) => {})
+  .catch((error) => console.log(error));
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,12 @@
 import 'reflect-metadata';
-import './database';
-
 import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
+app.use(express.json());
+app.use(router);
+// app.use(express.urlencoded({ extended: false }));
 
 app.listen(3001, () => console.log('Server is running!.'));
